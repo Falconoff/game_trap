33241871b3591b6c14c55df723b793d5214afd06
Falconoff <sokolovserega1982@gmail.com>
2024-04-22 21:21:23 +0300

    alfa testing

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 const messageTexts = [
   [
-    "Вы сейчас стоите на квадрате 0. По диагонали ходить нельзя! Ходить можно только вертикально и горизонтально.",
+    "ПРАВИЛА ИГРЫ: По диагонали ходить нельзя! Ходить можно только вертикально и горизонтально. Играем!... Нажмите Далее",
 
-    "Сделайте 5 ходов в любых направлениях, кроме как по диагонали. Запомните на каком квадрате остановились!",
+    "Вы сейчас стоите на квадрате 0. Сделайте 5 ходов в любых направлениях, кроме как по диагонали. Запомните на каком квадрате остановились! Нажмите Далее",
   ],
 
   [
     "Стартовый квадрат 0 нам уже не пригодится. Убираем его...",
-    "Сделайте ещё 4 хода. Не забудьте - по диагонали ходить нельзя! Запомните на каком квадрате остановились",
+    "С того места, где остановились, сделайте ещё 4 хода. Не забудьте - по диагонали ходить нельзя! Запомните свой квадрат",
   ],
 
   [
     "Я знаю, что Вас тут нет.",
-    "Сделайте ещё 3 хода. Не забудьте - по диагонали ходить нельзя! Запомните на каком квадрате остановились",
+    "Сделайте ещё 3 хода. По диагонали ходить нельзя! Запомните на каком квадрате остановились",
   ],
 
   [
     "Я знаю, что Вас тут нет. Я слежу за глазами ))",
-    "Сделайте ещё 3 хода. Не забудьте - по диагонали ходить нельзя! Запомните на каком квадрате остановились",
+    "Сделайте ещё 1 ход.",
   ],
 
-  [
-    "Я знаю, что Вас тут нет",
-    "Сделайте ещё 3 хода. Не забудьте - по диагонали ходить нельзя! Запомните на каком квадрате остановились",
-  ],
+  ["Я знаю, что Вас и тут нет", "Сделайте ещё 3 хода."],
 
-  "Я знаю где Вы! )))",
+  ["Я знаю где Вы! )))"],
+];
+
+const cellsToHide = [
+  [],
+  [9],
+  [0, 8],
+  [7],
+  [2, 6],
+  [1, 3, 5],
 ];
 
 const refs = {
@@ -34,8 +40,9 @@ const refs = {
   cells: document.querySelectorAll(".cell"),
 };
 
-const step = 0;
+let step = 1;
 
+// only for the first click
 refs.nextBtn.addEventListener(
   "click",
   onNextBtnClickFirstTime,
@@ -44,7 +51,8 @@ refs.nextBtn.addEventListener(
   },
 );
 
-console.log(refs.cells[9]);
+// console.log(refs.cells);
+// console.log(refs.cells[9]);
 
 refs.message.innerHTML = messageTexts[0][0];
 
@@ -58,26 +66,33 @@ function onNextBtnClickFirstTime() {
 
 function onNextBtnClick() {
   console.log("click another");
+
   onBtnDisabled(refs.nextBtn);
 
-  refs.message.innerHTML = messageTexts[1][0]; // mess: del cell
+  refs.message.innerHTML = messageTexts[step][0]; // mess: del cell
 
   // hide cell
   window.setTimeout(() => {
-    refs.cells[9].classList.add("hidden");
+    // refs.cells[9].classList.add("hidden");
+    hideCells(cellsToHide[step]);
   }, 2000);
-  // window.clearTimeout(timeoutID);
+
+  if (step >= 5) {
+    return;
+  }
 
   window.setTimeout(() => {
-    //  refs.cells[9].classList.add("hidden");
-    refs.message.innerHTML = messageTexts[1][1]; // mess: make steps
+    refs.message.innerHTML = messageTexts[step][1]; // mess: make steps
   }, 4000);
 
   window.setTimeout(() => {
     onBtnEnabled(refs.nextBtn);
+    step += 1;
   }, 6000);
 
-  // refs.cells[9].classList.add("hidden");
+  console.log("step=", step);
+
+  console.log("step=", step);
 }
 
 function onBtnDisabled(btn) {
@@ -88,4 +103,10 @@ function onBtnDisabled(btn) {
 function onBtnEnabled(btn) {
   btn.removeAttribute("disabled");
   btn.classList.remove("hidden");
+}
+
+function hideCells(arr) {
+  arr.forEach(index =>
+    refs.cells[index].classList.add("hidden"),
+  );
 }
