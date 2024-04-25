c2322aa22090de334dc576cfdab9b9a77fdee08b
Falconoff <sokolovserega1982@gmail.com>
2024-04-25 22:16:34 +0300

    add languages switcher

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const messageTexts = [
+// const messageTexts = [
+const messagesRus = [
   [
     "ПРАВИЛА ИГРЫ: 1 ход - это переход с одной клетки на другую (смежную). По диагонали ходить нельзя! Ходить можно только вертикально и горизонтально. Ходим взглядом, нажимать по клеткам не нужно. Играем!... Нажмите кнопку Далее",
 
@@ -21,12 +22,81 @@ const messageTexts = [
   ],
 
   [
-    "Я знаю, что Вас и тут нет. Я слежу за глазами ))",
+    "Я знаю, что Вас и тут нет. Я слежу за Вашими глазами ))",
     "Сделайте ещё 3 хода.",
   ],
 
   ["Я знаю где Вы! )))"],
 ];
+
+const messagesEng = [
+  [
+    "RULES OF THE GAME: 1 move is a transition from one cell to another (adjacent). You can't walk diagonally! You can only walk vertically and horizontally. We walk with our eyes, no need to click on the cells. Let's play!... Click Next",
+
+    "You are now standing on the square 0. Make 5 moves in any direction except diagonally, and remember which square you stopped on. Then click Next",
+  ],
+
+  [
+    "Starting square 0 will no longer be useful to us. I'll remove it",
+    "From the square where you are standing, make another 4 moves. Don't forget - You can't walk diagonally! Remember your square and click Next",
+  ],
+
+  [
+    "I know where you are not) Removing unnecessary squares",
+    "Make 3 more moves. You can't walk diagonally! Remember which square you stopped at and click Next",
+  ],
+
+  [
+    "I will again remove the square where you are not",
+    "Make 1 more move.",
+  ],
+
+  [
+    "I know that you are not here either. I'm watching your eyes ))",
+    "Make 3 more moves.",
+  ],
+
+  ["I know where you are! )))"],
+];
+
+const messagesUkr = [
+  [
+    "ПРАВИЛА ГРИ: 1 хід - це перехід з однієї клітинки на іншу (суміжну). По діагоналі ходити не можна! Ходити можна тільки вертикально та горизонтально. Ходимо поглядом, натискати по клітинкам не потрібно. Граємо!... Натисніть кнопку Далі",
+
+    "Ви зараз стоїте на квадраті 0. Зробіть 5 ходів в будь-яких напрямках, окрім по діагоналі, та запам'ятайте на якому квадраті зупинилися. Тоді натисніть кнопку Далі",
+  ],
+
+  [
+    "Стартовий квадрат 0 нам вже не знадобиться. Я прибираю його",
+    "З того квадрату, де стоїте, зробіть ще 4 ходи. Не забудьте - по діагоналі ходити не можна! Запам'ятайте свій квадрат і натисніть Далі",
+  ],
+
+  [
+    "Я знаю де Вас немає) Прибираю непотрібні квардати",
+    "Зробіть ще 3 ходи. По діагоналі ходити не можна! Запам'ятайте на якому квадраті зупинилися квадрат і натисніть Далі",
+  ],
+
+  [
+    "Я знову заберу квадрат, де Вас немає",
+    "Сделайте ещё 1 ход.",
+  ],
+
+  [
+    "Я знаю, що Вас і тут немає. Я слідкую за Вашими очима ))",
+    "Зробіть ще 3 ходи.",
+  ],
+
+  ["Я знаю де Ви! )))"],
+];
+
+const interfaceLang = {
+  eng: ["Reload", "Next"],
+  ukr: ["Спочатку", "Далі"],
+  rus: ["Сначала", "Далее"],
+};
+console.log(interfaceLang.ukr[1]);
+
+let messageTexts = messagesEng;
 
 const cellsToHide = [
   [],
@@ -40,7 +110,9 @@ const cellsToHide = [
 const refs = {
   message: document.querySelector(".instructions"),
   nextBtn: document.querySelector(".js-next-btn"),
+  reloadBtn: document.querySelector(".js-reload-btn"),
   cells: document.querySelectorAll(".cell"),
+  langBtns: document.querySelector(".js-lang-list"),
 };
 
 let step = 1;
@@ -55,6 +127,8 @@ refs.nextBtn.addEventListener(
     once: true,
   },
 );
+
+refs.langBtns.addEventListener("click", onLangBtnClick);
 
 // console.log(refs.cells);
 // console.log(refs.cells[9]);
@@ -128,4 +202,44 @@ function showSmoothInstructions(text) {
     refs.message.innerHTML = text;
     refs.message.classList.remove("instructions-hiding");
   }, 1000);
+}
+
+// ============ LANGUAGES ===================
+let currentLang = null;
+
+function onLangBtnClick(event) {
+  if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+
+  //  проверяем есть ли уже на какой-то кнопке активный класс
+  const currentActiveBtn = document.querySelector(
+    ".js-lang-list .active",
+  );
+  // если есть, то удаляем активный класс
+  currentActiveBtn?.classList.remove("active");
+
+  // добавляем активный класс на кликнутую кнопку
+  const newActiveBtn = event.target;
+  newActiveBtn.classList.add("active");
+
+  // ...и считываем значение из data-атрибута его значение
+  currentLang = newActiveBtn.dataset.lang;
+
+  console.log("click");
+  console.log("currentLang", currentLang);
+
+  switch (currentLang) {
+    case "ukr":
+      messageTexts = messagesUkr;
+      break;
+
+    case "rus":
+      messageTexts = messagesRus;
+      break;
+
+    default:
+      messageTexts = messagesEng;
+      break;
+  }
 }
